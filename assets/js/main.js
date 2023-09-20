952d4f54c3c5db5f0438aef97c97a26c4eb0e64a
milzinha <camilacarvalhoescritora@gmail.com>
2023-09-20 08:10:30 -0300

    Primeira versão finalizada, falta ajustes.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// let form = document.querySelector("#contactform");
+
+// form.addEventListener("submit", async (e) => {
+// 	e.preventDefault();
+// 	try {
+// 		await fetch(
+// 			"https://docs.google.com/forms/d/e/1FAIpQLSeeOM3sH38SmwPmDvMcHN-MULWR8WjJFbx2RDfOUXYLORZ2gw/formResponse",
+// 			{
+// 				method: "POST",
+// 				body: new FormData(form),
+// 			}
+// 		);
+// 	} catch (error) {
+// 		alert(error);
+// 	}
+// });
