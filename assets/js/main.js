6b428f6833562e7e1fdbc4f39bcb296fee36dd58
milzinha <camilacarvalhoescritora@gmail.com>
2023-09-22 08:51:43 -0300

    Ajuste no Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const botoes = document.querySelectorAll(".boximg");
 botoes.forEach((boximg) => {
 	boximg.addEventListener("mouseover", () => {
 		// Aplica o efeito de redimensionamento ao botão quando o mouse passa por cima
-		boximg.style.transform = "scale(1.1)"; // Aumenta o tamanho em 20%
+		boximg.style.transform = "scale(1.02)"; // Aumenta o tamanho em 20%
 	});
 
 	boximg.addEventListener("mouseout", () => {
